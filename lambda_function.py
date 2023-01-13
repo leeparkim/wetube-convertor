0031858983e9d39982f9b9cd78e1d2043ee21ee1
eunseo9808 <eunseo9808@naver.com>
2023-01-13 23:24:00 +0900

    feat: implement file upload to s3

--- lambda_function.py
+++ lambda_function.py
@@ -8,16 +8,34 @@ S3_DESTINATION_BUCKET = "wetube-eunseo"
 
 
 def handler(event, context):
-    s3_source_signed_url = "https://wetube-eunseo.s3.ap-northeast-2.amazonaws.com/17011557.mp4"
+    video_id = event['video_id']
+    filename = event['filename']
+    file_ext = event['file_ext']
+    s3_source_signed_url = f"https://wetube-eunseo.s3.ap-northeast-2.amazonaws.com/{video_id}/{filename}.{file_ext}"
+
+    os.makedirs(f"/tmp/{video_id}", exist_ok=True)
 
     s3_client = boto3.client('s3')
-    ffmpeg_cmd = "ffmpeg -i \"" + s3_source_signed_url + "\" -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls /tmp/filename.m3u8"
-    command1 = shlex.split(ffmpeg_cmd)
-    p1 = subprocess.run(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ffmpeg_cmd = f"ffmpeg -i \"{s3_source_signed_url}\" -codec: copy -start_number 0 -hls_time 10 " \
+                 f"-hls_list_size 0 -hls_segment_filename \'/tmp/{video_id}/{filename}_%d.ts\' " \
+                 f"-f hls /tmp/{video_id}/{filename}.m3u8"
 
-    resp = s3_client.put_object(Body=p1.stdout, Bucket=S3_DESTINATION_BUCKET, Key="filename.m3u8")
+    command1 = shlex.split(ffmpeg_cmd)
+    p1 = subprocess.run(command1)
+
+    upload_file_names = []
+    for (sourceDir, dirname, filename) in os.walk(f"/tmp/{video_id}"):
+        upload_file_names.extend(filename)
+        break
+
+    for filename in upload_file_names:
+        source_path = os.path.join(f"/tmp/{video_id}", filename)
+        s3_path = os.path.join(f"{video_id}", filename)
+        s3_client.upload_file(source_path, S3_DESTINATION_BUCKET, s3_path)
 
     return {
         'statusCode': 200,
         'body': json.dumps('Processing complete successfully')
     }
+
+
